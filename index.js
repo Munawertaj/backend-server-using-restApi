4d6eee4fd58ef8dfd364dfbacd7bb0140ed6dfe5
Munawer Taj <taj4580@gmail.com>
2022-03-08 21:01:32 +0600

    Index.js file updated

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
 const express = require("express");
 
-//Router Imports
 const dishRouter = require("./routes/dishRouter");
 const promoRouter = require("./routes/promoRouter");
 const leaderRouter = require("./routes/leaderRouter");
 
-//creating express app
 const app = express();
 
-//body Parser setup
 app.use(express.json());
 
-//Setup Routers
 app.use("/dishes", dishRouter);
 app.use("/promotions", promoRouter);
 app.use("/leaders", leaderRouter);
 
-//Listening
 app.listen(3000, () => {
-  console.log("Listening on port 3000....");
+  console.log("Listening on port 3000...");
 });
