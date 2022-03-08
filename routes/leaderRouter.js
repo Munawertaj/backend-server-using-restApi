dd3509a31c3c2107b4ad4804729c534b3cf8bd12
Munawer Taj <taj4580@gmail.com>
2022-03-08 21:47:14 +0600

    leaderRouter.jsr file updated

--- routes/leaderRouter.js
+++ routes/leaderRouter.js
@@ -1,50 +1,39 @@
 const express = require("express");
-
-//Initializing express Router Object
 const leaderRouter = express.Router();
 
-//Get Router
+
 leaderRouter.get("/", (req, res) => {
-  res.send(`will send all leaders`);
+  res.send(`Will send all leaders to You`);
 });
 
 leaderRouter.get("/:leaderId", (req, res) => {
   res.send(`Will send leader of id: ${req.params.leaderId}`);
 });
 
-//post Router
 leaderRouter.post("/", (req, res) => {
-  res.send(
-    `This will add leader: ${req.body.name} with details: ${req.body.description}`
-  );
+  res.send(`Will add the leader: ${req.body.name} with details: ${req.body.description}`);
 });
 
 leaderRouter.post("/:leaderId", (req, res) => {
-  res
-    .status(403)
-    .send(`Post request not supported on /leaders/${req.params.leaderId}`);
+  res.status(403).send(`Post operation is not supported on /leaders/${req.params.leaderId}`);
 });
 
-//Put Router
 leaderRouter.put("/:leaderId", (req, res) => {
   res.write(`Updating the leader: ${req.params.leaderId}`);
-  res.end(
-    `will update the leader: ${req.body.name} with details: ${req.body.description}`
-  );
+  res.end(`Will update the leader: ${req.body.name} with details: ${req.body.description}`);
 });
 
 leaderRouter.put("/", (req, res) => {
-  res.status(403).send(`put request not supported on /leaders`);
+  res.status(403).send(`Put operation is not supported on /leaders`);
 });
 
-//Delete Router
 leaderRouter.delete("/", (req, res) => {
-  res.send(`will delete all leaders`);
+  res.send(`Will delete all the leaders`);
 });
 
 leaderRouter.delete("/:leaderId", (req, res) => {
-  res.send(`will delete the leader of id: ${req.params.leaderId}`);
+  res.send(`Will delete the leader of id: ${req.params.leaderId}`);
 });
 
-//Export leaderRouter
+
 module.exports = leaderRouter;
